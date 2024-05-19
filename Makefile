e650e00bdd6caf3bf92d1ebd6c2f0e5d47018d2e
Marat Salakhov <salakhov.marat@gmail.com>
2024-05-19 19:16:08 +0500

    Added graphql

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
+ifeq (, $(shell which docker-compose))
+	DOCKER_COMPOSE := $(shell echo docker compose)
+else
+	DOCKER_COMPOSE := $(shell echo docker-compose)
+endif
+
 composer-install:
-	docker compose exec php-cli composer install -vv -o
+	$(DOCKER_COMPOSE) exec php-cli composer install -vv -o
+
+database-create:
+	$(DOCKER_COMPOSE) exec php-cli php bin/console doctrine:database:create --no-interaction
+
+migration-migrate:
+	$(DOCKER_COMPOSE) exec php-cli php bin/console doctrine:migration:migrate --no-interaction
--- app/src/Entity/Expense/Expense.php
+++ app/src/Entity/Expense/Expense.php
@@ -3,34 +3,44 @@ declare(strict_types=1);
 
 namespace App\Entity\Expense;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GraphQl\Mutation;
+use ApiPlatform\Metadata\GraphQl\Query;
+use ApiPlatform\Metadata\GraphQl\QueryCollection;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 
+#[ApiResource(graphQlOperations: [
+    new Query(),
+    new QueryCollection(),
+    new Mutation(name: 'create')
+])]
 #[Entity]
 class Expense
 {
     #[Id]
     #[GeneratedValue]
     #[Column(type: 'bigint')]
-    private ?int $id;
+    private ?string $id;
 
     #[Column(type: 'string')]
     private string $name;
 
     #[Column(type: 'datetime_immutable')]
-    private \DateTimeImmutable $implementationDate;
+    private DateTimeImmutable $implementationDate;
 
     #[Column(type: 'decimal', options: ['precision' => 20, 'scale' => 2])]
     private string $amount;
 
-    public function getId(): ?int
+    public function getId(): ?string
     {
         return $this->id;
     }
 
-    public function setId(?int $id): self
+    public function setId(?string $id): self
     {
         $this->id = $id;
 
@@ -49,12 +59,12 @@ class Expense
         return $this;
     }
 
-    public function getImplementationDate(): \DateTimeImmutable
+    public function getImplementationDate(): DateTimeImmutable
     {
         return $this->implementationDate;
     }
 
-    public function setImplementationDate(\DateTimeImmutable $implementationDate): self
+    public function setImplementationDate(DateTimeImmutable $implementationDate): self
     {
         $this->implementationDate = $implementationDate;
 
